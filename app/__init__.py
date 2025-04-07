ca3280f75adf61ee58305a0f368bc562f581730c
Yashvardhan Arekapudi <143351150+ArekaYash@users.noreply.github.com>
2025-04-07 10:04:18 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from app import routes
 import os
 
@@ -6,9 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     # Use the $PORT environment variable to bind to the correct port
     port = int(os.environ.get("PORT", 8000))  # Default to 8000 for local testing
     app.run(host="0.0.0.0", port=port)
+
